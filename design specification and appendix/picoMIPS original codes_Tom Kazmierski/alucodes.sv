eec78c34a8b16e4995c4dd4ec1d8b5f790eac3af
lx2u16 <lx2u16@soton.ac.uk>
2018-08-16 11:25:14 +0100

    add design sepcification and appendix

---
+++ design specification and appendix/picoMIPS original codes_Tom Kazmierski/alucodes.sv
@@ -0,0 +1,15 @@
+//-----------------------------------------------------
+// File Name : alucodes.sv
+// FuncRon : pMIPS ALU funcRon code definiRons
+// Author: tjk
+// Last rev. 23 Oct 12
+//-----------------------------------------------------
+//
+`define RA 3'b000
+`define RB 3'b001
+`define RADD 3'b010
+`define RSUB 3'b011
+`define RAND 3'b100
+`define ROR 3'b101
+`define RXOR 3'b110
+`define RNOR 3'b111
